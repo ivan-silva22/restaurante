f90713c478af3a4ea7f9023de396d4ef6c2a7505
Ivan Silva <silvaivan280@gmail.com>
2024-01-18 16:18:11 -0300

    realize la instancia de Producto

--- js/administrador.js
+++ js/administrador.js
@@ -1,3 +1,5 @@
+import Producto from "./classProducto.js";
+
 let formularioProducto = document.getElementById('form-producto');
 
 formularioProducto.addEventListener('submit', prepararFormulario);
@@ -5,4 +7,12 @@ formularioProducto.addEventListener('submit', prepararFormulario);
 function prepararFormulario(e){
     e.preventDefault();
     console.log('hola mundo')
+    const nuevoProducto = new Producto(
+        'Pizza',
+        'Plato principal',
+        'https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg',
+        4500,
+        'Activo'
+    )
+    console.log(nuevoProducto)
 }
---
+++ js/classProducto.js
@@ -0,0 +1,57 @@
+export default class Producto{
+    #codigo;
+    #nombre;
+    #categoria;
+    #imagen;
+    #precio;
+    #estado;
+    constructor(nombre, categoria, imagen, precio, estado){
+        this.#codigo = 0;
+        this.#nombre = nombre;
+        this.#categoria = categoria;
+        this.#imagen = imagen;
+        this.#precio = precio;
+        this.#estado = estado;
+    }
+    get codigo(){
+        return this.#codigo;
+    }
+    set codigo(nuevoCodigo){
+        this.#codigo = nuevoCodigo;
+    }
+
+    get nombre(){
+        return this.#nombre;
+    }
+    set nombre(nuevoNombre){
+        this.#nombre = nuevoNombre;
+    }
+
+    get categoria(){
+        return this.#categoria;
+    }
+    set categoria(nuevaCategoria){
+        this.#categoria = nuevaCategoria;
+    }
+
+    get imagen(){
+        return this.#imagen;
+    }
+    set imagen(nuevaImagen){
+        this.#imagen = nuevaImagen;
+    }
+
+    get precio(){
+        return this.#precio;
+    }
+    set precio(nuevoPrecio){
+        this.#precio = nuevoPrecio;
+    }
+
+    get estado(){
+        return this.#estado;
+    }
+    set estado(nuevoEstado){
+        this.#estado = nuevoEstado;
+    }
+}
