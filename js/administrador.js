353afc1daf911f9d9df824b42c7140a3dedec5ec
Ivan Silva <silvaivan280@gmail.com>
2024-01-25 16:26:11 -0300

    agregue el read de productos

--- js/administrador.js
+++ js/administrador.js
@@ -2,21 +2,64 @@ import Producto from "./classProducto.js";
 import { resumenValidacion } from "./helpers.js";
 
 let formularioProducto = document.getElementById("form-producto");
-let listaProductos = [];
+let listaProductos = JSON.parse(localStorage.getItem("listaProductos")) || [];
+console.log(listaProductos);
+if (listaProductos.length !== 0) {
+  listaProductos = listaProductos.map(
+    (producto) =>
+      new Producto(
+        producto.nombre,
+        producto.categoria,
+        producto.imagen,
+        producto.precio,
+        producto.estado
+      )
+  );
+}
+console.log(listaProductos);
 let codigo = document.getElementById("codigo"),
   nombre = document.getElementById("nombre"),
   categoria = document.getElementById("categoria"),
   imagen = document.getElementById("imagen"),
   precio = document.getElementById("precio"),
   estado = document.getElementById("estado");
-let objetoModal = new bootstrap.Modal(
-  document.getElementById("modal")
-);
+let objetoModal = new bootstrap.Modal(document.getElementById("modal"));
 
 let btnCrearProducto = document.getElementById("btnCrearProducto");
 
 formularioProducto.addEventListener("submit", prepararFormulario);
 btnCrearProducto.addEventListener("click", mostrarFormModal);
+
+cargarInicial();
+
+function cargarInicial() {
+  if (listaProductos.length > 0) {
+    listaProductos.map((producto) => crearFila(producto));
+  }
+}
+
+function crearFila(producto) {
+  let tbody = document.getElementById("tablaProdutos");
+  console.log(tbody)
+  tbody.innerHTML += `<tr>
+  <th scope="row">1</th>
+  <td>${producto.nombre}</td>
+  <td>${producto.categoria}</td>
+  <td class="text-truncate tamanio-celda">
+    ${producto.imagen}
+  </td>
+  <td>${producto.precio} </td>
+  <td>${producto.estado} </td>
+  <td>
+    <button class="btn btn-danger">
+      <i class="bi bi-trash3"></i>
+    </button>
+    <button class="btn btn-warning">
+      <i class="bi bi-pencil-square"></i>
+    </button>
+  </td>
+</tr>`;
+}
 
 function prepararFormulario(e) {
   e.preventDefault();
@@ -58,5 +101,5 @@ export function limpiarFormulario() {
 
 function mostrarFormModal() {
   objetoModal.show();
-  objetoModal.backdrop
+  objetoModal.backdrop;
 }
