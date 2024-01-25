8a2f3ea816b7ee4225e4922eba00a1f4b6dd7365
Ivan Silva <silvaivan280@gmail.com>
2024-01-25 17:30:12 -0300

    agregue el read de la categoria bebidas

--- js/menuBebidas.js
+++ js/menuBebidas.js
@@ -1,2 +1,47 @@
-let listaProductos = JSON.parse(localStorage.setItem('listaProductos'));
+import Producto from "./classProducto.js";
+
+let listaProductos = JSON.parse(localStorage.getItem("listaProductos"));
+console.log(listaProductos);
+
+if (listaProductos.length > 0) {
+  listaProductos = listaProductos.map(
+    (producto) =>
+      new Producto(
+        producto.nombre,
+        producto.categoria,
+        producto.imagen,
+        producto.precio,
+        producto.estado
+      )
+  );
+}
+
 console.log(listaProductos)
+
+cargarInicial()
+
+function cargarInicial(){
+    if(listaProductos.length > 0){
+        listaProductos.map((producto) => crearCard(producto));
+    }
+}
+
+function crearCard(producto){
+    console.log(producto.categoria)
+    if(producto.categoria === 'bebidas'){
+        let row = document.getElementById('row');
+        row.innerHTML += `<div class="col-md-4 col-sm-12 card-menu">
+        <div class="card-img">
+            <img class="img-fluid" src=${producto.imagen} alt="">
+        </div>
+        <div class="card-bg ">
+            <h5>${producto.nombre} </h5>
+            <p>$${producto.precio}</p>
+            <button class="btn">Agregar a pedidos</button>
+        </div>
+      </div>`
+    }let row = document.getElementById("row");
+    row.innerHTML = `<div class='col col-sm-12'>
+    <h1 class="texto-menu text-center">Noy hay productos</h1>
+    </div>`;
+}
